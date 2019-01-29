0100a556c8002c8f53750b86d607e66da3387c0b
abbiejones <abjones@pdx.edu>
2019-01-29 00:08:29 -0800

    initial angular 6 front end add

---
+++ frontend/BankingLedger/src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+//import { AuthGuard }                          from './auth/auth.guard';
+//import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
+
+export const appRoutes: Routes = [
+  {
+      path: 'login',
+      component: LoginComponent
+  },
+  {
+      path: 'register',
+      component: RegisterComponent
+  },
+  {
+      path: '',
+      redirectTo: '/login',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/login',
+      pathMatch: 'full'
+  }
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        enableTracing: false, // <-- debugging purposes only
+        //preloadingStrategy: SelectivePreloadingStrategyService,
+      }
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
+
